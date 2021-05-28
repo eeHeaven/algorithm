b686ced49152f9be7e37587a6c83abee6017609c
heaven <porori0703@ewhain.net>
2021-05-28 12:31:08 +0900

    sort버전 추가

---
+++ hash/완주하지못한선수sort.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+         String answer = "";
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        
+        for (int i = 0; i<completion.length; i++){
+            if(!completion[i].equals(participant[i])) {answer = participant[i]; return answer;}
+        }
+        answer = participant[participant.length - 1];
+        return answer;
+    }
+}
