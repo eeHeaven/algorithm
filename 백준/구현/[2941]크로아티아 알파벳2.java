3238a8069b34d707ba9a89ce2e84daa62311d04c
Hyebin Lee <84822464+eeHeaven@users.noreply.github.com>
2021-08-13 14:00:34 +0900

    다른 풀이(알파벳을 *로 치환하는 경우)

---
+++ 백준/구현/[2941]크로아티아 알파벳2.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter; 
+import java.io.IOException; 
+import java.io.InputStreamReader; 
+import java.io.OutputStreamWriter; 
+public class Ex2941_1 { 
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    
+    String[] word = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="}; 
+    String input = br.readLine().trim(); 
+    int result = 0; 
+    
+    int i; 
+    for(i=0; i<word.length; i++) {
+      if(input.contains(word[i])) {
+        input = input.replaceAll(word[i], "*"); // System.out.println(input);
+      } } 
+    
+    result = input.length(); 
+    bw.write(result + ""); 
+    bw.flush(); 
+    br.close(); 
+    bw.close(); } }​
