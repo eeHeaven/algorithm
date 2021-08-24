60a4b3b10197df1b55959c576dafeec297eb7b7e
Hyebin Lee <84822464+eeHeaven@users.noreply.github.com>
2021-08-24 19:39:55 +0900

    Create [10828]스택.java

---
+++ 백준/자료구조/[10828]스택.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Main{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        Stack<Integer> st = new Stack<>();
+        int orders = Integer.parseInt(br.readLine());
+        for(int i = 0; i<orders; i++){
+            int num = 0;
+            String key = br.readLine();
+            if(key.contains("push")){
+                String[] pushs = key.split(" ");
+                num = Integer.parseInt(pushs[1]);
+                key = "push";
+            }
+            switch(key){
+                case "push":
+                    st.push(num);
+                    break;
+                case "pop":
+                    if(st.isEmpty()) {System.out.println(-1); break;}
+                    else{
+                        System.out.println(st.pop()); break;}
+                case "size":
+                    System.out.println(st.size());
+                    break;
+                case "empty":
+                    if(st.isEmpty()) System.out.println(1);
+                    else System.out.println(0);
+                    break;
+                case "top":
+                    if(st.isEmpty()) System.out.println(-1);
+                    else System.out.println(st.peek());
+                    break;
+
+
+            }
+        }
+    }
+    }
