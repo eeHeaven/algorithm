bb75e5938bb07e0572d13c0140f918a165c4cd77
Hyebin Lee <84822464+eeHeaven@users.noreply.github.com>
2021-11-10 15:43:45 +0900

    Create 불량사용자(모범풀이).java

---
+++ 카카오기출/2019겨울인턴십/불량사용자(모범풀이).java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+
+    Set<Integer> set;
+
+    public int solution(String[] user_id, String[] banned_id) {
+        set = new HashSet<>();
+
+        go(0, user_id, banned_id, 0);
+
+        return set.size();
+    }
+
+    public void go(int index, String[] user_id, String[] banned_id, int bit) {
+
+        if(index == banned_id.length) {
+            set.add(bit);
+            return;
+        }
+
+        String reg = banned_id[index].replace("*", "[\\w\\d]");
+        for(int i=0; i<user_id.length; ++i) {
+            if((((bit>>i) & 1) == 1) || !user_id[i].matches(reg)) continue;
+            go(index + 1, user_id, banned_id, (bit | 1<<i));
+        }
+
+    }
+
+}
