adb9b325fa605160501a6848643ee48033db36e7
Hyebin Lee <84822464+eeHeaven@users.noreply.github.com>
2021-12-06 11:42:21 +0900

    DP를 이용한 다른 사람 풀이

---
+++ 백준/부르트포스 알고리즘/[14501]퇴사(DP).java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner s= new Scanner(System.in);
+		int N = s.nextInt();
+		int time[] = new int[N+1];
+		int pay[] = new int[N+1];
+		int dp[] = new int[N+2];
+	
+		for(int i=1;i<=N;i++) {
+			time[i]=s.nextInt();
+			pay[i]=s.nextInt();
+		}
+		
+		for(int i=N;i>0;i--) {
+			int index=i+time[i];
+			
+			if(index >N+1) 
+				dp[i]=dp[i+1];
+			else
+				dp[i]=Math.max(dp[i+1],pay[i]+dp[index]);
+			
+		}
+		
+			System.out.println(dp[1]);
+		
+	}
+}
