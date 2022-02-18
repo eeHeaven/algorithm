39bf31db6dcd3244b8a831e6dcbf35874b36b728
Hyebin Lee <84822464+eeHeaven@users.noreply.github.com>
2022-02-18 09:23:39 +0900

    나의풀이 (20)

---
+++ 백준/[문제집]코딩테스트 준비 - 브루트포스/N과M/[11727]2xn 타일링2.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+//백준 문제 한줄씩 읽어오는 방법은 String XX = br.readLine() 하면 됨
+//출력은 그냥 sout
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int n;
+    static int[] dp;
+
+
+    public static void main(String[] args) throws IOException {
+
+        String input = br.readLine();
+        n = Integer.parseInt(input);
+        dp = new int[n + 1];
+
+        dp[1] = 1;
+        if (n == 1) {
+            System.out.println(dp[1]);
+            return;
+        }
+        dp[2] = 3;
+        if (n == 2) {
+            System.out.println(dp[2]);
+            return;
+        }
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
+        }
+        System.out.println(dp[n]);
+        return;
+    }
+}
